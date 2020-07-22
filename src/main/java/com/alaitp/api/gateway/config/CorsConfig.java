0d13e2555bb9ee268cdc650b4fc822cbafe311b8
Junpeng.He <hejunpeng2012@hotmail.com>
2020-07-22 13:13:28 +0800

    feat(gateway): websocket gateway working

---
+++ src/main/java/com/alaitp/api/gateway/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.alaitp.api.gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+    @Value("${alaitp.frontend.uri}")
+    private String frontendUri;
+
+    @Bean
+    public CorsResponseHeaderFilter corsResponseHeaderFilter() {
+        return new CorsResponseHeaderFilter();
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.addAllowedOrigin(frontendUri);
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setMaxAge(600L);
+
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
