8e4ff5b9191b1651d0f41791ca51e10d02fa010a
Junpeng.He <hejunpeng2012@hotmail.com>
2020-08-06 15:35:24 +0800

    build(config): config file reformat

--- src/main/java/com/alaitp/api/gateway/config/RateLimiter.java
+++ src/main/java/com/alaitp/api/gateway/config/RateLimiter.java
@@ -10,6 +10,6 @@ import reactor.core.publisher.Mono;
 public class RateLimiter {
     @Bean
     KeyResolver pathKeyResolver() {
-        return exchange -> Mono.just(exchange.getRequest().getPath().toString());
+        return exchange -> Mono.just(exchange.getRequest().getPath().value());
     }
 }
