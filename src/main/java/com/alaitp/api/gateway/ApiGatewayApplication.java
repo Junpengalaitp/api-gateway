c3a60802a62ef34052da55bb647255d54ed60594
Junpeng He <hejunpeng2012@hotmail.com>
2021-01-03 15:31:21 +0800

    config(k8s): use k8s config map for api-gateway

--- src/main/java/com/alaitp/api/gateway/ApiGatewayApplication.java
+++ src/main/java/com/alaitp/api/gateway/ApiGatewayApplication.java
@@ -2,10 +2,12 @@ package com.alaitp.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableConfigurationProperties
+@EnableDiscoveryClient
 @SpringBootApplication
-@EnableEurekaClient
 public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
