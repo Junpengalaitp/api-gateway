e45c193500e24ebde98e95d3406a02b3c281609d
Junpeng He <hejunpeng2012@hotmail.com>
2021-02-08 14:28:21 +0800

    build(k8s): change k8s service type

--- package.py
+++ package.py
@@ -39,9 +39,30 @@ def change_config_file():
 
     print "using " + env_yml_path + " as config file"
 
+def restore_config_file():
+    print "restore config file to dev"
+
+    bootstrap_yml = open("src/main/resources/bootstrap.yml", "w")
+    bootstrap_yml.truncate()
+
+    dev_yml = "bootstrap-dev.yml"
+
+    dev_yml_path = "src/main/resources/" + dev_yml
+    dev_yml_file = open(dev_yml_path)
+
+    for line in dev_yml_file:
+        bootstrap_yml.write(line)
+
+    bootstrap_yml.close()
+    dev_yml_file.close()
+
+    print "using " + dev_yml_path + " as config file"
+
 def package_jar():
+    change_config_file()
     run_cmd("mvn clean")
     run_cmd("mvn package -Dmaven.test.skip=true")
+    restore_config_file()
 
 def build_image():
     run_cmd("docker build --tag=" + app_name + " --force-rm=true .")
@@ -50,7 +71,6 @@ def build_image():
 
 def k8s_deploy():
     run_cmd("kubectl apply -f kubernetes.yaml")
-
 
 def run_sudo_cmd(cmd):
     cmd = "sudo " + cmd
@@ -64,7 +84,6 @@ def run_cmd(cmd):
 if __name__ == '__main__':
     git_pull()
     get_ssl_cert()
-    change_config_file()
     package_jar()
     build_image()
     k8s_deploy()
